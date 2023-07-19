26238e042742da94004132885e896930f5bb5ff2
Carlos <39968271+charlesfig@users.noreply.github.com>
2023-07-19 13:07:35 +0200

    Feedback changes

--- app/page.tsx
+++ app/page.tsx
@@ -73,31 +73,46 @@ const grid_works: Array<WorkProps> = [
     image: { src: "/images/funeral_march.jpeg", height: 930, width: 750 },
     publication: "Lobster Magazine",
   },
+  {
+    name: "Of moon and earth",
+    image: { src: "/images/funeral_march.jpeg", height: 930, width: 750 },
+    publication: "Lobster Magazine",
+  },
 ];
 
-const Work = ({ name, image, publication, className = "" }: WorkProps & { className?: string }) => (
+const Work = ({
+  name,
+  image,
+  publication,
+  className = "",
+  imgClassName = "",
+}: WorkProps & { className?: string; imgClassName?: string }) => (
   <div id="work" className={`relative w-full group overflow-hidden ${className}`}>
     <div className="w-full bg-black/20 invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-all h-full absolute top-0 p-2 flex flex-col items-end justify-end text-white">
       <p className="uppercase text-right">{name}</p>
       <p className="italic text-right text-sm opacity-80 ">{publication}</p>
     </div>
-    <Image className="w-full max-w-xl mx-auto" alt={`"${name}" by Alexia Planas Lee`} {...image} />
+    <Image
+      className={`w-full max-w-xl mx-auto ${imgClassName}`}
+      alt={`"${name}" by Alexia Planas Lee`}
+      {...image}
+    />
   </div>
 );
 
 export default function Homepage() {
   return (
     <div className="w-full text-center relative max-w-xl mx-auto p-4">
-      <header className="sticky top-0 left-0 py-10 bg-white z-10">
-        <h1 className="text-center text-4xl md:text-5xl font-medium uppercase">
+      <header className="sticky top-0 left-0 py-5 bg-white z-10">
+        <h1 className="text-center text-4xl md:text-5xl font-medium uppercase tracking-[0.35px]">
           Alexia Planas Lee
         </h1>
-        <p className="tracking-wider uppercase">
+        <p className="tracking-wider uppercase mt-1">
           Creative Direction | Fashion Styling | Production
         </p>
-        <div className="text-right absolute -left-14 top-36 flex flex-col gap-2 w-12">
-          {["Contact", "About", "Works"].map((item) => (
-            <div className="hover:font-bold hover:scale-105 transition-all cursor-pointer">
+        <div className="text-right absolute -left-20 top-36 flex flex-col gap-2 w-16">
+          {["About", "Works", "Contact"].map((item) => (
+            <div className="hover:font-bold cursor-pointer uppercase scale-[0.8] text-[17px]">
               {item}
             </div>
           ))}
@@ -106,17 +121,10 @@ export default function Homepage() {
 
       <div id="wrapper" className="w-full">
         {/* Main Work */}
-        <Work {...main_work} />
-
-        {/* Works Grid */}
-        <div className="mt-4 grid grid-cols-2 gap-2">
-          {grid_works.map((work) => (
-            <Work {...work} className="aspect-square" />
-          ))}
-        </div>
+        <Work {...main_work} imgClassName="-mt-[26px]" />
 
         {/* Description */}
-        <div className="mt-10 w-full text-left">
+        <div className="my-10 w-full text-justify">
           Raised in Barcelona, I'm a Spanish and Chinese visual creative with work showcased in
           Vogue Italia, Vanidad, and New York City's Times Square. As Head of Impact Design and
           Innovation at a Web3 fashion circularity startup, I have a competitive understanding of
@@ -124,6 +132,16 @@ export default function Homepage() {
           industry - blockchain product transparency, Al, metaverse experiences (VR AR), and digital
           twins.
         </div>
+
+        {/* Works Grid */}
+        <div className="grid grid-cols-2 gap-2">
+          {grid_works.map((work) => (
+            <Work {...work} className="aspect-square" />
+          ))}
+        </div>
+
+        {/* Contact Info */}
+        <div className="my-20 w-full text-center">contact@alexiaplanaslee.com</div>
       </div>
     </div>
   );
