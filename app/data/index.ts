1d44fec280c5bbbbf1f02d2bed45d729c937ef9e
Carlos <39968271+charlesfig@users.noreply.github.com>
2023-07-20 19:08:29 +0200

    Refactor data

---
+++ app/data/index.ts
@@ -0,0 +1,48 @@
+import { Work } from "../types";
+
+export const grid_works: Array<Work> = [
+  {
+    photo_id: 1,
+    project_id: 1,
+    project_name: "Dinner for none",
+    image: { src: "/images/dinner_for_none/1.jpg", height: 2507, width: 2079 },
+    publication: "PETRIe Inventory",
+  },
+  {
+    photo_id: 2,
+    project_id: 2,
+    project_name: "Funeral march of a marionette",
+    image: { src: "/images/funeral_march_of_a_marionette/1.jpg", height: 1271, width: 977 },
+    publication: "No Future Magazine",
+    className: "object-top",
+  },
+  {
+    photo_id: 3,
+    project_id: 3,
+    project_name: "So you are, so you love",
+    image: { src: "/images/so_you_are_so_you_love/1.jpg", height: 2678, width: 2143 },
+    publication: "Odalisque Magazine | Vogue Italia’s PhotoVogue",
+    className: "object-top",
+  },
+  {
+    photo_id: 4,
+    project_id: 4,
+    project_name: "Of moon and earth",
+    image: { src: "/images/of_moon_and_earth/1_1.jpg", height: 2395, width: 1903 },
+    publication: "Lobster Magazine",
+  },
+  {
+    photo_id: 5,
+    project_id: 5,
+    project_name: "Isolation",
+    image: { src: "/images/isolation/1.JPG", height: 1024, width: 1435 },
+    publication: "Lobster Magazine",
+  },
+  {
+    photo_id: 6,
+    project_id: 6,
+    project_name: "Forward motion",
+    image: { src: "/images/forward_motion/3.jpg", height: 2835, width: 1889 },
+    publication: "Luxiders",
+  },
+];
